2fdcfa315894c35968b4f5f9b35e856bcf755252
= <=>
2021-10-01 10:34:32 +0200

    adaptation 1(do not run)

---
+++ src/main/java/com/saphjyr/eicons/EiconsMod.java
@@ -0,0 +1,35 @@
+package com.saphjyr.eicons;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.ModInitializer;
+
+
+public class EiconsMod implements ModInitializer
+{
+    
+    @Environment(EnvType.CLIENT)
+    @Override
+    public void onInitialize() 
+    {
+        /*
+        ArmorVenchants.RegisterLeatherHelmet();
+        ArmorVenchants.RegisterLeatherBoot();
+        ArmorVenchants.RegisterLeatherArmor();
+        ArmorVenchants.RegisterHelmet();
+        ArmorVenchants.RegisterBoot();
+        ArmorVenchants.RegisterArmor();
+        
+        HandheldVenchants.RegisterSwords();
+        HandheldVenchants.RegisterTools();
+       
+        ProjectilethrowerVenchants.RegistrerBow();
+        ProjectilethrowerVenchants.RegisterCrossbow();
+
+        FishingRodVenchants.RegisterFishingRod();
+        
+        TridentVenchants.RegisterTrident(); */
+    }
+    
+}
+    
